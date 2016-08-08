cfcf59fb1a6410fd4bd701fdbe93ad107813e5d0
Colicino <ec3168@mc.cumc.columbia.edu>
2016-08-08 13:51:45 -0400

    comments to Roxygen style

--- code/meth02_analyze_data.R
+++ code/meth02_analyze_data.R
@@ -15,6 +15,7 @@ suppressPackageStartupMessages({
   library(data.table)
   library(qqman)
   library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+  library(DMRcate)
 })
 
 ## predict gender from methylation
@@ -38,60 +39,16 @@ pheno[,"Sex"]<-ifelse(pheno[,"Sex"]==2,0,1)
 # 1 female, 0 male
 
 ## Cleaning up the methylation data
-## upload annotation file
-data(IlluminaHumanMethylation450kanno.ilmn12.hg19)
-IlluminaAnnot = data.frame(
-  chr=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Locations$chr,
-  pos=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Locations$pos,
-  Relation_to_Island=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Islands.UCSC$Relation_to_Island,
-  UCSC_RefGene_Name=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Other$UCSC_RefGene_Name,
-  UCSC_RefGene_Group=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Other$UCSC_RefGene_Group,
-  Probe_rs=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$SNPs.137CommonSingle$Probe_rs,
-  Probe_maf=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$SNPs.137CommonSingle$Probe_maf,
-  CpG_rs=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$SNPs.137CommonSingle$CpG_rs,
-  CpG_maf=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$SNPs.137CommonSingle$CpG_maf,
-  SBE_rs=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$SNPs.137CommonSingle$SBE_rs,
-  SBE_maf=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$SNPs.137CommonSingle$SBE_maf,
-  Probe_SNPs_10=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$SNPs.Illumina$Probe_SNPs_10,
-  probeA=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Manifest$ProbeSeqA,
-  probeB=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Manifest$ProbeSeqB,
-  Forward_Sequence=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Other$Forward_Sequence,
-  SourceSeq=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Other$SourceSeq)
-
-## Create CpG name and annotate row names
-rownames(IlluminaAnnot) <- rownames(IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Manifest)
-IlluminaAnnot$Name <-rownames(IlluminaAnnot)
-dim(IlluminaAnnot)
-
-# IlluminaAnnotation flag non-CpG (i.e "rs" and "ch" probes)
-IlluminaAnnot$NONCPG <- 1*(substring(rownames(IlluminaAnnot),1,2)=="ch") 
-table(IlluminaAnnot$NONCPG)  # 3,091  ch probes
-IlluminaAnnot = IlluminaAnnot[IlluminaAnnot$NONCPG==0,] 
-dim(IlluminaAnnot)
-
-IlluminaAnnot$SNP <- 1*(substring(rownames(IlluminaAnnot),1,2)=="rs") 
-table(IlluminaAnnot$SNP) # 0 actual SNP Data
-dim(IlluminaAnnot)
-
-## Remove probes with SNPs within 10 bps of the targetsite
-IlluminaAnnot<-subset(IlluminaAnnot, IlluminaAnnot$Probe_SNPs_10=="")
-dim(IlluminaAnnot)
-
-#Note: Additional cleaning steps could be considered 
-
-# Intersect with clean betas 
-intersect = intersect(rownames(IlluminaAnnot), rownames(betas.rcp))
-length(intersect)
-betas.clean = betas.rcp[intersect,]
-IlluminaAnnot = IlluminaAnnot [intersect,]
-dim(betas.clean)
-dim(IlluminaAnnot)
+#' Filters a matrix of beta values by distance to SNP. Also removes crosshybridising probes and sex-chromosome probes.
+betas.clean<-rmSNPandCH(betas.rcp,  mafcut = 0.05, and = TRUE, rmcrosshyb = TRUE, rmXY= TRUE)
+nCpG <- dim(betas.clean)[1]
+nCpG
 
 #'# Running an Epigenome Wide Association
 #' Here we run an EWAS on sex (as a predictor of methylation)
 
 #' first we can run a linear regression on a single cpg
-j <- 2744
+j <- 133211
 
 CpG.level <- betas.clean[j,]
 CpG.name <- rownames(betas.clean)[j]
@@ -110,8 +67,6 @@ boxplot(CpG.level ~ pheno[,"Sex"])
 summary(lm(CpG.level~pheno[,"Sex"]))
 
 #' EWAS and results 
-nCpG <- dim(betas.clean)[1]
-nCpG
 
 #' sex as predictor  
 #' note that CpGassoc is quite fast for running almost a half million regressions!
@@ -125,6 +80,9 @@ cbind(results1$coefficients[j,4:5], results1$results[j,c(1,3)])
 
 #' Bonferroni significant hits
 table(results1$results[,3] < 0.05/(nCpG))
+#' FDR significant hits
+table(results1$results[,5] < 0.05)
+
 
 #' EWAS with adjustment for cell types
 results2 <- cpg.assoc(betas.clean,pheno[,"Sex"], 
@@ -134,11 +92,14 @@ results2 <- cpg.assoc(betas.clean,pheno[,"Sex"],
 
 #' look at the results
 head(cbind(results2$coefficients[,4:5], P.value=results2$results[,3])[order(results2$results[,3]),])
+#' Bonferroni significant hits
 table(results2$results[,3] < 0.05/(nCpG))
+#' FDR significant hits
+table(results2$results[,5] < 0.05)
 
 #' qqplot and lambda interpretation  
 #+ fig.width=13, fig.height=7, dpi=300
-par(mfrow=c(1,2))
+par(mfrow=c(1,1))
 #plot(results1, main="QQ plot for association between methylation and sex")
 plot(results2, main="QQ plot for association between methylation and sex \n adjusted for cells proportion")
 
@@ -156,19 +117,36 @@ plot(results2$coefficients[,4],-log10(results2$results[,3]),
      xlab="Estimate", ylab="-log10(Pvalue)", main="Volcano Plot- adj for CellProp")
 #Bonferroni treshold
 abline(h = -log10(0.05/(nCpG)), lty=1, col="red", lwd=2)
+#FDR treshold
+abline(h = -log10(max(results1$results[results1$results[,5] < 0.05,3])), lty=1, col="blue", lwd=2)
 
 #'## Manhattan plot for cell-type adjusted EWAS  
 #' the function manhattan needs data.frame including CpG, Chr, MapInfo and Pvalues
+data(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+IlluminaAnnot = data.frame(
+  chr=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Locations$chr,
+  pos=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Locations$pos,
+  Relation_to_Island=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Islands.UCSC$Relation_to_Island,
+  UCSC_RefGene_Name=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Other$UCSC_RefGene_Name,
+  UCSC_RefGene_Group=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Other$UCSC_RefGene_Group,
+  probeA=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Manifest$ProbeSeqA,
+  probeB=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Manifest$ProbeSeqB,
+  Forward_Sequence=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Other$Forward_Sequence,
+  SourceSeq=IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Other$SourceSeq)
+
+## Create CpG name and annotate row names
+rownames(IlluminaAnnot) <- rownames(IlluminaHumanMethylation450kanno.ilmn12.hg19@data$Manifest)
+IlluminaAnnot$Name <-rownames(IlluminaAnnot)
+dim(IlluminaAnnot)
+
+IlluminaAnnot = IlluminaAnnot [intersect(rownames(IlluminaAnnot), rownames(betas.clean)),]
+dim(IlluminaAnnot)
 datamanhat <- data.frame(CpG=results2$results[,1],Chr=as.character(IlluminaAnnot$chr),
                      Mapinfo=IlluminaAnnot$pos,Pval=results2$results[,3])
 #' Reformat the variable Chr (so we can simplify and use a numeric x-axis)
-datamanhat$Chr <- sub("chr","",datamanhat$Chr)
-datamanhat$Chr[which(datamanhat$Chr=="X")] <- "23"
-datamanhat$Chr[which(datamanhat$Chr=="Y")] <- "24"
-datamanhat$Chr <- as.numeric(datamanhat$Chr)
+datamanhat$Chr <- as.numeric(sub("chr","",datamanhat$Chr))
 
-#' Manhattan plot (from CpGassoc package)
-par(mfrow=c(1,1))
+#' Manhattan plot 
 manhattan(datamanhat,"Chr","Mapinfo", "Pval", "CpG", main="Manhattan Plot - adj for CellProp")
 
 #' End of script 02
