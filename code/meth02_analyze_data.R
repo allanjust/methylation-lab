4fb6bdad33bb130c3c8eec7dc56b295413a59ace
Allan Just <allanjust@gmail.com>
2016-08-08 11:12:45 -0400

    comments to Roxygen style

--- code/meth02_analyze_data.R
+++ code/meth02_analyze_data.R
@@ -80,9 +80,9 @@ dim(IlluminaAnnot)
 #Note: Additional cleaning steps could be considered 
 
 # Intersect with clean betas 
-intersect = intersect(rownames(IlluminaAnnot), rownames(betas.bmiq))
+intersect = intersect(rownames(IlluminaAnnot), rownames(betas.rcp))
 length(intersect)
-betas.clean = betas.bmiq[intersect,]
+betas.clean = betas.rcp[intersect,]
 IlluminaAnnot = IlluminaAnnot [intersect,]
 dim(betas.clean)
 dim(IlluminaAnnot)
@@ -90,15 +90,15 @@ dim(IlluminaAnnot)
 #'# Running an Epigenome Wide Association
 #' Here we run an EWAS on sex (as a predictor of methylation)
 
-# first we can run a linear regression on a single cpg
-j <-2744
+#' first we can run a linear regression on a single cpg
+j <- 2744
 
 CpG.level <- betas.clean[j,]
 CpG.name <- rownames(betas.clean)[j]
 CpG.name
 
-# difference in methylation between different Sex
-# some statistics
+#' difference in methylation between different Sex
+#' some statistics
 knitr::kable(cbind(Min=round(simplify2array(tapply(CpG.level, pheno[,"Sex"],min)),3),
       Mean=round(simplify2array(tapply(CpG.level, pheno[,"Sex"],mean)),3), 
       Median=round(simplify2array(tapply(CpG.level, pheno[,"Sex"],median)),3),
@@ -109,59 +109,65 @@ knitr::kable(cbind(Min=round(simplify2array(tapply(CpG.level, pheno[,"Sex"],min)
 boxplot(CpG.level ~ pheno[,"Sex"])
 summary(lm(CpG.level~pheno[,"Sex"]))
 
-## EWAS and results 
+#' EWAS and results 
 nCpG <- dim(betas.clean)[1]
 nCpG
 
-# sex as predictor
-results1 <- cpg.assoc(betas.clean,pheno[,"Sex"])
+#' sex as predictor  
+#' note that CpGassoc is quite fast for running almost a half million regressions!
+system.time(results1 <- cpg.assoc(betas.clean,pheno[,"Sex"]))
 
-#look at the results
+#' look at the results
 head(cbind(results1$coefficients[,4:5], P.value=results1$results[,3]))
 head(cbind(results1$coefficients[,4:5], P.value=results1$results[,3])[order(results1$results[,3]),])
-#check with previous results
+#' check with previous results
 cbind(results1$coefficients[j,4:5], results1$results[j,c(1,3)])
 
-#Bonferroni significant hits
-table(results1$results[,3]<0.05/(nCpG))
+#' Bonferroni significant hits
+table(results1$results[,3] < 0.05/(nCpG))
 
-#EWAS with adjustment for cell types
+#' EWAS with adjustment for cell types
 results2 <- cpg.assoc(betas.clean,pheno[,"Sex"], 
                     covariates=pheno[,"CD8T"]+ pheno[,"CD4T"] +  
                       pheno[,"NK"]  + pheno[,"Bcell"] + 
                       pheno[,"Mono"] + pheno[,"Gran"] + pheno[,"nRBC"])
 
-#look at the results
+#' look at the results
 head(cbind(results2$coefficients[,4:5], P.value=results2$results[,3])[order(results2$results[,3]),])
-table(results2$results[,3]<0.05/(nCpG))
+table(results2$results[,3] < 0.05/(nCpG))
 
-#' qqplot and lambda interpretation
+#' qqplot and lambda interpretation  
 #+ fig.width=13, fig.height=7, dpi=300
 par(mfrow=c(1,2))
 #plot(results1, main="QQ plot for association between methylation and sex")
 plot(results2, main="QQ plot for association between methylation and sex \n adjusted for cells proportion")
 
-# Lambda
+#' Lambda - this is a summary of genomic inflation  
+#' ratio of observed vs expected median p-value - is there early departure of the qqline?  
+#' estimated at -log10(0.5) ~ 0.3 on the x-axis of a qqplot  
 lambda <- function(p) median(qchisq(p, df=1, lower.tail=FALSE), na.rm=TRUE) / qchisq(0.5, df=1)
+#' Lambda for the first EWAS
 lambda(results1$results[,3])
+#' Lambda after the cell type adjustment
 lambda(results2$results[,3])
 
 # Volcano Plot-results2
-plot(results2$coefficients[,4],-log10(results2$results[,3]), xlab="Estimate", ylab="-log10(Pvalue)", main="Volcano Plot- adj for CellProp")
+plot(results2$coefficients[,4],-log10(results2$results[,3]), 
+     xlab="Estimate", ylab="-log10(Pvalue)", main="Volcano Plot- adj for CellProp")
 #Bonferroni treshold
-abline(h=-log10(0.05/(nCpG)), lty=1, col="red", lwd=2)
+abline(h = -log10(0.05/(nCpG)), lty=1, col="red", lwd=2)
 
-# Manhattan plot -results2
-#the function manhattan needs data.frame including CpG, Chr, MapInfo and Pvalues
+#'## Manhattan plot for cell-type adjusted EWAS  
+#' the function manhattan needs data.frame including CpG, Chr, MapInfo and Pvalues
 datamanhat <- data.frame(CpG=results2$results[,1],Chr=as.character(IlluminaAnnot$chr),
                      Mapinfo=IlluminaAnnot$pos,Pval=results2$results[,3])
-#Reformat the variable Chr
+#' Reformat the variable Chr (so we can simplify and use a numeric x-axis)
 datamanhat$Chr <- sub("chr","",datamanhat$Chr)
 datamanhat$Chr[which(datamanhat$Chr=="X")] <- "23"
 datamanhat$Chr[which(datamanhat$Chr=="Y")] <- "24"
 datamanhat$Chr <- as.numeric(datamanhat$Chr)
 
-#manhattan plot
+#' Manhattan plot (from CpGassoc package)
 par(mfrow=c(1,1))
 manhattan(datamanhat,"Chr","Mapinfo", "Pval", "CpG", main="Manhattan Plot - adj for CellProp")
 
