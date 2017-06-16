25650f63280a40f820e18992667bb02e93cf8cc0
ElenaColicino <ecolicin@hsph.harvard.edu>
2017-06-16 15:33:26 -0400

    Update meth02_analyze_data.R

--- code/meth02_analyze_data.R
+++ code/meth02_analyze_data.R
@@ -12,6 +12,7 @@ knitr::opts_knit$set(root.dir = "../")
 load("~/BootCamp_Epigenetics/Data/WB.noob.RData")
 dim(WB.noob)
 cellprop<-read.csv("~/BootCamp_Epigenetics/Data/cellprop_WB_20samps_EPICdemo.csv")
+load("~/BootCamp_Epigenetics/Data/betas.rcp.RData")
 
 #' load packages
 # To Fix lirary IlluminaHumanMethylation450kanno.ilmn12.hg19
