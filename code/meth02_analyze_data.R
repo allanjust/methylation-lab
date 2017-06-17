d1d33973faf0b1cea467e90bb7ffd516c4ee1381
ElenaColicino <ecolicin@hsph.harvard.edu>
2017-06-17 09:40:25 -0400

    Update meth02_analyze_data.R

--- code/meth02_analyze_data.R
+++ code/meth02_analyze_data.R
@@ -13,7 +13,7 @@ load("~/BootCamp_Epigenetics/Data/WB.noob.RData") # phenotype data
 dim(WB.noob)
 cellprop<-read.csv("~/BootCamp_Epigenetics/Data/cellprop_WB_20samps_EPICdemo.csv") # cell type composition
 load("~/BootCamp_Epigenetics/Data/betas.rcp.RData") # processed betas
-load("~/BootCamp_Epigenetics/Data/Gbeta.RData")
+load("~/BootCamp_Epigenetics/Data/Gbeta.RData") # annotation
 
 #' load packages
 # To Fix lirary IlluminaHumanMethylation450kanno.ilmn12.hg19
