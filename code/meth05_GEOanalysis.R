a3b51a0e0f7980fefefc980851193921b14fe767
Unknown <jonathan.heiss@mssm.edu>
2018-05-03 17:32:36 -0400

    Updated exemplary analysis of GEO dataset

--- code/meth05_GEOanalysis.R
+++ code/meth05_GEOanalysis.R
@@ -4,78 +4,83 @@ library(utils)
 library(data.table)
 library(stringi)
 library(minfi)
+library(purrr)
+library(magrittr)
 
-#+ setdir05, echo = F
-knitr::opts_knit$set(root.dir = "../")
+# Investigating a 450k dataset from [Gene Expression Omnibus](https://www.ncbi.nlm.nih.gov/geo/) by NCBI
+# another great option is [ArrayExpress](https://www.ebi.ac.uk/arrayexpress/browse.html) from the European EMBL-EBI 
+# 
+# Read about the dataset at https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE72556
+# more info here from the publication: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5223358/
 
-#' Investigating a 450k dataset from [Gene Expression Omnibus](https://www.ncbi.nlm.nih.gov/geo/) by NCBI
-#' another great option is [ArrayExpress](https://www.ebi.ac.uk/arrayexpress/browse.html) from the European EMBL-EBI 
-#' 
-#' Read about the dataset at https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE72556
-#' more info here from the publication: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5223358/
+dir.create("GSE72556")
 
-dir.create("data/GSE72556")
+# Download meta data (copy URL in browser to see what the requested file looks like)
+meta = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE72556&targ=gsm&form=text&view=brief"
+meta = readLines(meta)
 
-#' Either download manually from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE72556 (look for GSE72556_RAW.tar under supplementary files)
-#' or run this command in R
-download.file(url="ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE72nnn/GSE72556/suppl/GSE72556_RAW.tar",
-              destfile="data/GSE72556/GSE72556_RAW.tar")
+# Split into single samples
+i = which(meta %like% "^\\^SAMPLE = GSM")
+i2 = rep(1:(length(i)-1),times=diff(i))
+i2 = c(i2,rep(length(i),times=length(meta)-i[length(i)]+1))
+meta = split(meta,i2)
+rm(i,i2)
 
-#' Extract the archive, again either manually, or by running 
-untar("data/GSE72556/GSE72556_RAW.tar", exdir = "data/GSE72556")
+# Extract GSM accessions
+accessions = map(meta,1) %>% stri_match_first(regex="GSM\\d+")
+names(meta) = accessions; rm (accessions)
 
-#' Now download the meta data (list of samples and characteristics) from ...
-#' https://gbnci-abcc.ncifcrf.gov/geo/
-#' ... by searching for GSE72556 and retrieving the list of samples associated with this GEO series.
-#' When you have the list of samples, click "Display options" and add "Characteristics Ch1" to the list of selected fields.
-#' Download the .csv file and place it in the same folder.
+imap(meta,function(s,acc){
+	s = strsplit(s,split=" = ",fixed=TRUE)	
+	data.table(gsm=acc,variable=map_chr(s,1),value=map_chr(s,2))
+}) -> meta
 
-#' Import the .csv file into R. The filename includes the current date, so edit the code below accordingly.
-if(length(list.files(path = "data/GSE72556", pattern = "GEOmetadb_download.*\\.csv")) == 1){
-    geometafile <- list.files(path = "data/GSE72556", pattern = "GEOmetadb_download.*\\.csv", full.names = TRUE)
-  } else {
-    warning(paste("did you download the metadata and put it in this directory?", file.path(getwd(), "data/GSE72556/")))
-  }
-meta <- fread(geometafile, sep=",")
+meta = rbindlist(meta)
 
-if(!any(grepl("characteristics", names(meta)))){
-  warning("characteristics field is missing - did it get added before download from GEOmetadb?")}
+# Keep only information on sample characteristics and supplementary files
+meta = meta[variable %chin% c("!Sample_characteristics_ch1","!Sample_supplementary_file")]
+i = meta[variable == "!Sample_characteristics_ch1",which=TRUE]
+ch = meta$value[i] %>% stri_split(fixed=": ")
+meta$variable[i] = map_chr(ch,1)
+meta$value   [i] = map_chr(ch,2)
+rm(ch)
 
-#' Parse the sample characteristics using regular expressions
-meta[,adult_age   :=stri_match(characteristics_ch1,regex="adult age: (\\d+);"       )[,2]]
-meta[,adult_bmi   :=stri_match(characteristics_ch1,regex="adult bmi: ([0-9\\.]+);"  )[,2]]
-meta[,adult_waist :=stri_match(characteristics_ch1,regex="adult waist: ([0-9\\.]+);")[,2]]
-meta[,child_gender:=stri_match(characteristics_ch1,regex="child gender: ([FM]);"    )[,2]]
-meta[,child_age   :=stri_match(characteristics_ch1,regex="child age: (\\d);"        )[,2]]
-meta[,child_bmi   :=stri_match(characteristics_ch1,regex="child bmi: ([0-9\\.]+);"  )[,2]]
-meta[,child_waist :=stri_match(characteristics_ch1,regex="child waist: ([0-9\\.]+)$")[,2]]
+# Find the FTP URLs linking to the two .idat files
+meta[variable == "!Sample_supplementary_file" & value %like% "_Red\\.idat",variable:="red"]
+meta[variable == "!Sample_supplementary_file" & value %like% "_Grn\\.idat",variable:="grn"]
 
-#' Cast variables from type 'character' to 'numeric'
-meta[,adult_age  :=as.numeric(adult_age)  ]
-meta[,adult_bmi  :=as.numeric(adult_bmi)  ]
-meta[,adult_waist:=as.numeric(adult_waist)]
-meta[,child_age  :=as.numeric(child_age)  ]
-meta[,child_bmi  :=as.numeric(child_bmi)  ]
-meta[,child_waist:=as.numeric(child_waist)]
+# Reshape data.table from long to wide format
+meta = dcast(meta, gsm ~ variable)
 
-#' Determine the basenames of the .idat files required for minfi 'read.metharray'
-meta[,basename:=paste0("data/GSE72556/", geo_accession, "_", title)]
+# Keep only the first ten samples
+meta = meta[1:10]
 
-#' Drop all the irrelevant columns
-meta = meta[,list(adult_age,adult_bmi,adult_waist,child_gender,child_age,child_bmi,child_waist,basename)]
+# Download .idat files
+lapply(meta$red,function(file) download.file(url=file,dest=file.path("GSE72556",basename(file)) ))
+lapply(meta$grn,function(file) download.file(url=file,dest=file.path("GSE72556",basename(file)) ))
 
-#' Drop rows with missing
-sgithubmeta = na.omit(meta)
+# File path for read.metharray
+meta[,file:=file.path("GSE72556",basename(red)) %>% stri_sub(1,-13)]
+meta$red = NULL; meta$grn = NULL
 
-#' Import the methylation data
-rgset = read.metharray(meta$basename)
+# Type casting
+meta$`adult age`    %<>% as.numeric
+meta$`adult bmi`    %<>% as.numeric
+meta$`adult waist`  %<>% as.numeric
+meta$`child age`    %<>% as.numeric
+meta$`child bmi`    %<>% as.numeric
+meta$`child waist`  %<>% as.numeric
+meta$`child gender` %<>% factor
 
-#' Usually we should normalize the data, but we skip this step for now to save time
+# Import the methylation data
+rgset = read.metharray(meta$file)
+
+# Usually we should normalize the data, but we skip this step for now to save time
 beta = getBeta(preprocessRaw(rgset)) 
 
-#' Run a linear regression for the first 100 probes, store the p-values for 'adult_bmi'
+# Run a linear regression for the first 100 probes, store the p-values for 'adult_bmi'
 pvals = apply(beta[1:100,],1,function(x){
-	coef(summary(lm(x ~ adult_bmi+adult_age+adult_waist+child_gender+child_age+child_bmi+child_waist,data=meta)))['adult_bmi','Pr(>|t|)']
+	coef(summary(lm(x ~ `adult bmi`+`adult age`+`adult waist`+`child gender`+`child age`+`child bmi`+`child waist`,data=meta)))["`adult bmi`","Pr(>|t|)"]
 })
 
 pvals
