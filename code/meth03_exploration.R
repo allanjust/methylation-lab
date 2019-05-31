cd15dad86c5d3a99662c87517d384a5d57c3c995
Unknown <jonathan.heiss@mssm.edu>
2019-05-31 13:32:49 -0400

    Restore erroneously overwritten code

--- code/meth03_exploration.R
+++ code/meth03_exploration.R
@@ -1,28 +1,67 @@
+library(data.table)
+suppressMessages(library(ENmix))
+
+#' Load the data
+load("data/processed.rda")
+
+#'# Exploring global DNA Methylation variability via PCs
+# Let's look at the effect of sex
+#+ fig.width=8, fig.height=6, dpi=300
+plotMDS(beta, top=10000, gene.selection="common",
+        pch=17,col=c("deeppink","blue")[factor(pheno$sex)],
+        dim=c(1,2),cex=1.5)
+legend("center", legend=levels(factor(pheno$sex)),bty='n',
+       cex=1.5,pch=17,col=c("deeppink","blue"))
+
+# Let's look at the effect of sex but let's remove the X and Y chromosomes
+betas.clean = beta[manifest[probe_type=="cg" & !chr %in% c("X","Y")]$index,]
+plotMDS(betas.clean, top=10000, gene.selection="common",
+        pch=17,col=c("deeppink","blue")[factor(pheno$sex)],
+        dim=c(1,2),cex=1.5)
+legend("center", legend=levels(factor(pheno$sex)),bty='n',
+       cex=1.5,pch=17,col=c("deeppink","blue"))
+
+#' It would be useful to look at several traits with global variability
+cov<-data.frame(pheno[,2:9])
+npc <- 20 # Top 20 PCs
+svd <- prcomp(t(na.omit(betas.clean)))
+screeplot(svd, npc, type = "barplot")
+eigenvalue <- svd[["sdev"]]^2
+prop <- (sum(eigenvalue[1:npc])/sum(eigenvalue)) * 100
+cat("Top ", npc, " principal components can explain ", 
+    prop, "% of data \n    variation", "\n")
+screeplot(svd,npc,type="barplot")
+pcrplot(na.omit(betas.clean), cov, npc=5)
+
+
+#'# Epigenetic Age
 #'Load package for Age-Prediction
 library(wateRmelon)
-DNAmAge<-as.vector(agep(betas.clean))
+DNAmAge<-as.vector(agep(beta))
+hist(DNAmAge)
+boxplot(DNAmAge);stripchart(DNAmAge, vertical = T,method = "jitter", add = T, pch = 20, col = 'red')
 
-#'Agreement between chronological age and DNAm-Age
+#'Agreement between Horvath's Epigenetic age and Hannum's clock
+data(hannumCoef)
+length(hannumCoef)
+DNAmAge.Hannum<-as.vector(agep(beta,coeff=hannumCoef,method = "hannum"))
 
-#' Correlation
-cor.test(DNAmAge,pheno$AGE)
-plot(pheno$AGE,DNAmAge,pch=21,ylab="Age Predicted",
-     xlab="Age Reported",cex=1.2, bg=alpha("deepskyblue",0.45),main="Prediction of Age")
-legend("topleft",legend=paste0("r=",round(cor(DNAmAge,pheno$AGE),2)),bty="n")
-abline(lm(DNAmAge~pheno$AGE),col="red",lw=2)
+
+#' Correlation; agreement
+cor.test(DNAmAge,DNAmAge.Hannum)
+plot(DNAmAge.Hannum,DNAmAge,pch=21,ylab="Hortvath's DNAm Age",
+     xlab="Hannum's DNAm Age",cex=1.2, bg=alpha("deepskyblue",0.45),main="Epigenetic Clocks")
+legend("topleft",legend=paste0("r=",round(cor(DNAmAge.Hannum,DNAmAge),2)),bty="n")
+abline(lm(DNAmAge~DNAmAge.Hannum),col="red",lw=2)
 
 
 #' Age Acceleration Residuals
-AgeAccelerationResidual<-residuals(lm(DNAmAge~pheno$AGE))
-boxplot(AgeAccelerationResidual ~pheno$SMOKE_STATUS, col=c("red","blue"))
-wilcox.test(AgeAccelerationResidual ~ pheno$SMOKE_STATUS)
-t.test(AgeAccelerationResidual ~ pheno$SMOKE_STATUS)
+AgeAccelerationResidual<-residuals(lm(DNAmAge.Hannum~DNAmAge))
+boxplot(AgeAccelerationResidual ~pheno$smoker, col=c("blue","red"))
+wilcox.test(AgeAccelerationResidual ~ pheno$smoker)
 
-
-#' Online Calculator
-Horvath<-read.csv("C:/EBC3/Data/MethylationData.output.csv")
-cor.test(Horvath$DNAmAge,pheno$AGE)
-plot(pheno$AGE,Horvath$DNAmAge,pch=21,ylab="Age Predicted",
-     xlab="Age Reported",cex=1.2, bg=alpha("deepskyblue",0.45),main="Prediction of Age")
-legend("topleft",legend=paste0("r=",round(cor(Horvath$DNAmAge,pheno$AGE),2)),bty="n")
-abline(lm(Horvath$DNAmAge~pheno$AGE),col="red",lw=2)
+#' Differences by Smoking status
+boxplot(DNAmAge ~pheno$smoker, col=c("blue","red"))
+wilcox.test(DNAmAge ~ pheno$smoker)
+boxplot(DNAmAge.Hannum ~pheno$smoker, col=c("blue","red"))
+wilcox.test(DNAmAge.Hannum ~ pheno$smoker)
